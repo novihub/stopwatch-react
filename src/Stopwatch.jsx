1addcd665d1c27181c370408efef4e51331fc3fc
novikoff <novikoff04@gmail.com>
2024-07-26 14:21:47 +0500

    new features

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -10,7 +10,7 @@ function Stopwatch() {
 		if (isRunning) {
 			intervalIdRef.current = setInterval(() => {
 				setElapsedTime(Date.now() - startTimeRef.current)
-			}, 10)
+			}, 1)
 		}
 
 		return () => {
@@ -33,12 +33,12 @@ function Stopwatch() {
 		let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
 		let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60)
 		let seconds = Math.floor((elapsedTime / 1000) % 60)
-		let milliseconds = Math.floor((elapsedTime % 1000) / 10)
+		let milliseconds = Math.floor((elapsedTime % 1000))
 
 		hours = String(hours).padStart(2, '0')
 		minutes = String(minutes).padStart(2, '0')
 		seconds = String(seconds).padStart(2, '0')
-		milliseconds = String(milliseconds).padStart(2, '0')
+		milliseconds = String(milliseconds).padStart(3, '0')
 
 		return `${hours}:${minutes}:${seconds}:${milliseconds}`
 	}
